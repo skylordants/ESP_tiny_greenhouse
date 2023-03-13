3165a87158fb428c469c75e465c4710afab83bcb
Ants Kärner <karner.ants@gmail.com>
2023-03-14 00:59:34 +0200

    Wifi connection and getting time via sntp

---
+++ include/networking.h
@@ -0,0 +1,9 @@
+#ifndef __NETWORKING_H__
+#define __NETWORKING_H__
+
+void init_networking();
+void init_wifi();
+void init_sntp();
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include "switch.h"
 #include "soil_moisture.h"
 #include "water_level.h"
+#include "networking.h"
+
+#include "time.h"
 
 void stuff (void *pvParameter) {
 	CapacitiveMoistureSensor soil {SOIL_MOISTURE_SENSOR_ADC_UNIT, SOIL_MOISTURE_SENSOR_ADC_CHANNEL};
@@ -17,7 +20,20 @@ void stuff (void *pvParameter) {
 	Switch pump {PUMP_CONTROL_PIN};
 	Switch relay {RELAY_CONTROL_PIN};
 
+	init_networking();
+	init_wifi();
+	init_sntp();
+
+	time_t now;
+    struct tm timeinfo;
+	char strftime_buf[64];
+
+
 	while (true) {
+		time(&now);
+		localtime_r(&now, &timeinfo);
+		strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+		printf("Time: %s\n", strftime_buf);
 		printf("Water Empty: %i\n", water.IsEmpty());
 		soil.measure();
 		relay.turn_on();
