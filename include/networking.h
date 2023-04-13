b99be4067dd782538b20e51c7d9ae89b69506909
Ants Kärner <karner.ants@gmail.com>
2023-04-13 15:19:23 +0300

    Restructure networking and add mqtt capabilities

--- include/networking.h
+++ include/networking.h
@@ -4,6 +4,11 @@
 void init_networking();
 void init_wifi();
 void init_sntp();
+void init_mqtt();
+
+int mqtt_publish(const char *topic, const char *data, int retain = 0, int qos = 1, int len = 0);
+int mqtt_subscribe(const char *topic, int qos = 1);
+int mqtt_unsubscribe(const char *topic);
 
 
 #endif
--- src/networking.cpp
+++ src/networking.cpp
@@ -1,8 +1,16 @@
 #include "networking.h"
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -10,31 +18,51 @@
 #include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_sntp.h"
-#include "lwip/ip_addr.h"
 
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "lwip/ip_addr.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
+#include "mqtt_client.h"
 
 
 #define ESP_WIFI_SSID      "campusnet"
 #define ESP_WIFI_PASS      ""
 #define ESP_MAXIMUM_RETRY  5
-
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+
 
 #define SNTP_SERVER "ee.pool.ntp.org"
 
+
+#define MQTT_SERVER "mqtt://g4.yt"
+#define MQTT_USER "greenhouse"
+#define MQTT_PASS "greenhouse"
+
+static const char *TAG = "NETWORKING";
+
+void init_networking() {
+	ESP_ERROR_CHECK( nvs_flash_init() );
+	ESP_ERROR_CHECK( esp_netif_init() );
+	ESP_ERROR_CHECK( esp_event_loop_create_default() );
+}
+
+
+
+// WIFI AND IP STUFF
 
 static EventGroupHandle_t s_wifi_event_group;
 
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
-static const char *TAG = "NETWORKING";
-
 static int s_retry_num = 0;
 
-
+// Event handler for WiFi and IP events
 static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
 	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -54,13 +82,6 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
 		s_retry_num = 0;
 		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
 	}
-}
-
-
-void init_networking() {
-	ESP_ERROR_CHECK( nvs_flash_init() );
-	ESP_ERROR_CHECK( esp_netif_init() );
-	ESP_ERROR_CHECK( esp_event_loop_create_default() );
 }
 
 void init_wifi() {
@@ -119,8 +140,12 @@ void init_wifi() {
 
 }
 
+
+// SNTP STUFF
+
+
 void init_sntp() {
-    ESP_LOGI(TAG, "Initializing and starting SNTP");
+	ESP_LOGI(TAG, "Initializing and starting SNTP");
 
 	sntp_setoperatingmode(SNTP_OPMODE_POLL);
 	sntp_setservername(0, "pool.ntp.org");
@@ -131,3 +156,72 @@ void init_sntp() {
 }
 
 
+// MQTT STUFF
+
+static esp_mqtt_client_handle_t mqtt_client;
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+	ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%ld", base, event_id);
+	esp_mqtt_event_handle_t event = static_cast<esp_mqtt_event_handle_t>(event_data);
+	switch (static_cast<esp_mqtt_event_id_t>(event_id)) {
+	case MQTT_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+		break;
+	case MQTT_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+		break;
+	case MQTT_EVENT_SUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_UNSUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_PUBLISHED:
+		//ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_DATA:
+		//ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+
+		//printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		//printf("DATA=%.*s\r\n", event->data_len, event->data);
+		break;
+	case MQTT_EVENT_ERROR:
+		ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+		if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+			ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+		}
+		break;
+	default:
+		ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+		break;
+	}
+}
+
+void init_mqtt() {
+	esp_mqtt_client_config_t mqtt_cfg {};
+	mqtt_cfg.broker.address.uri = MQTT_SERVER;
+	mqtt_cfg.credentials.username = MQTT_USER;
+	mqtt_cfg.credentials.authentication.password = MQTT_PASS;
+
+	mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+	esp_mqtt_client_register_event(mqtt_client, MQTT_EVENT_ANY, mqtt_event_handler, NULL);
+	esp_mqtt_client_start(mqtt_client);
+}
+
+int mqtt_publish(const char *topic, const char *data, int retain, int qos, int len) {
+	int msg_id =  esp_mqtt_client_publish(mqtt_client, topic, data, len, qos, retain);
+	//ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+	return msg_id;
+}
+
+int mqtt_subscribe(const char *topic, int qos) {
+	int msg_id = esp_mqtt_client_subscribe(mqtt_client, topic, qos);
+	ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+	return msg_id;
+}
+
+int mqtt_unsubscribe(const char *topic) {
+	int msg_id = esp_mqtt_client_unsubscribe(mqtt_client, topic);
+	ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+	return msg_id;
+}
